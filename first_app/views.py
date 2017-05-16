107ea38e3500ba7a27ae33614392f14dde3cc2b2
bilalahmad1994 <bilalshakeel590@gmail.com>
2017-05-16 21:12:48 +0500

    adding Httprequest and render with templates

--- first_app/views.py
+++ first_app/views.py
@@ -5,14 +5,17 @@ from django.http import HttpResponse
 # Create your views here.
 # def index(request):
 #     my_dict={'insert_me','Hello I am from views.py'}
-#     return render(request,'index.html',context=[])
-    # return HttpResponse('insert_me','HELLO')
+#     return render(request,'index.html',context=my_dict)
+#     return HttpResponse('insert_me','HELLO')
+#
 
 
+
+def help(request):
+    return HttpResponse('<em>Hello you are in help?</em>')
 
 def index(request):
     context = locals()
-
+    mydic={'insert_me',}
     templates = 'firstapp/index.html'
-
     return render(request, templates, context)
