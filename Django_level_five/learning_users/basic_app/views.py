22e147c3fd8f210938a1c48a9b006118728083f3
bilalahmad <bilalshakeel590@gmail.com>
2017-05-27 20:41:51 +0500

    implement login and logout

--- Django_level_five/learning_users/basic_app/views.py
+++ Django_level_five/learning_users/basic_app/views.py
@@ -2,10 +2,28 @@ from django.shortcuts import render
 from basic_app.forms import Userform,UserProfileInfoForm
 
 
+#Extra Imports for the Login and Logout capabilities
+from django.contrib.auth import authenticate,login,logout
+from django.http import HttpResponse,HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+
+
+
+
 
 # Create your views here.
 def index(request):
     return render(request,'basic_app/index.html')
+
+@login_required
+def special(request):
+    return HttpResponse('You are now Logged in')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
 
 def register(request):
@@ -50,6 +68,35 @@ def register(request):
     return render(request,'basic_app/registration.html', {'user_form':user_form,
                            'profile_form':profile_form,
                            'registered':registered})
+
+
+def user_login(request):
+    if request.method=='POST':
+
+        # we get the username and password supplied
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+
+#         Django authentication function
+        user=authenticate(username=username,password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponse(reverse('index'))
+            else:
+                return HttpResponseRedirect('Your account is not active')
+        else:
+            # if you provide for username or password
+            print('Someone tried to login ur accout and failed')
+            print('They used username:{} and password:{}'.format(username,password))
+            return HttpResponse('Invalid Login details')
+
+    else:
+        return render(request,'basic_app/login.html',{})
+
+
 
 
 
--- Django_level_five/learning_users/learning_users/urls.py
+++ Django_level_five/learning_users/learning_users/urls.py
@@ -21,6 +21,8 @@ from basic_app import views
 urlpatterns = [
     url(r'^$' ,views.index ,name='index'),
     url(r'^admin/', admin.site.urls),
+    url(r'^logout/',views.user_logout,name='logout'),
     url(r'^basic_app/',include('basic_app.urls')),
-
+    url(r'^special/',views.special,name='special'),
 ]
+
